e1bec24ac934563b39a5c31ebcd5f2a615266047
Paolo Valletta <paovalletta@hotmail.it>
2018-06-07 09:41:55 +0200

    Bugfix e aggiunti servizi. Porta spostata su 9090

--- collocamentoREST/src/main/java/CollocamentoREST/UserController.java
+++ collocamentoREST/src/main/java/CollocamentoREST/UserController.java
@@ -12,13 +12,18 @@ public class UserController {
 	
 	private UserDAO udao = new UserDAO();
 	
-	@GetMapping("/users")
+	@GetMapping("/users/all")
 	public List<User> retrieveAllStudents() {
-		return udao.gestListaUtenti();
+		return udao.getUsers();
 	}
-	
-	@GetMapping("/test")
-	public String test() {
-		return "ciao";
+
+	@GetMapping("/users/{cf}")
+	public User getUserById(@PathVariable("cf") String cf) {
+		return udao.getUterByCf(cf);
+	}
+
+	@PostMapping("/users/add")
+	public boolean addUser(@RequestBody User user) {
+		return udao.addUser(user);
 	}
 }
